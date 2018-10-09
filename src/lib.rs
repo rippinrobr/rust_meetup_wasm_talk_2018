6981dee55f63255ccc635a8443c84e106fece92d
Rob Rowe <rrowe@foodlogiq.com>
2018-10-09 17:58:42 -0400

    added debug settings to cargo file, and setup the nicer panic message handling

--- src/lib.rs
+++ src/lib.rs
@@ -20,6 +20,8 @@ extern "C" {
 
 #[wasm_bindgen]
 pub fn init() {
+    utils::set_panic_hook();
+    
     let window = web_sys::window().expect("no global `window` exists");
     let document = window.document().expect("should have a document on window");
     let delete_btn = document.get_element_by_id("delete-image").unwrap();
